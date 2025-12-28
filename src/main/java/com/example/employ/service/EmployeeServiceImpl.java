3f3866d0c3c220cf70f2980f0c93efa4210a6930
Vivek Kumar <kumarvivek77333@gmail.com>
2025-12-28 16:41:52 +0530

    add

--- src/main/java/com/example/employ/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/employ/service/EmployeeServiceImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
-public class EmployServiceImpl implements EmployServices {
+public class EmployeeServiceImpl implements EmployServices {
 
 
 
